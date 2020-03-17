a2e789940121daf5aebd8e322bc6d7a251e2a477
Hanna Waage Hjelmeland <hannawh@stud.ntnu.no>
2020-03-17 12:42:22 +0100

    Some cleanup in HandwrittenNumbers

--- HandwrittenNumbers/ascOrder.m
+++
@@ -1,7 +0,0 @@
-function newQueue = ascOrder(arg, list)
-    n = size(list, 1);
-    for i = 1:n
-        
-    end
-end
-
--- HandwrittenNumbers/num.m
+++ HandwrittenNumbers/num.m
@@ -2,19 +2,21 @@ k = 8;
 
 classified = zeros(num_test, 1);
 
-for i = 1:5 
-    kSmallest = NaN(2, k); %% [class i; distance]
-    kSmallest(2, :) = Inf;
+
+
+for i = 1:num_test
+    k_smallest = NaN(2, k); %% [class i; distance]
+    k_smallest(2, :) = Inf;
     x = testv(i, :);
     for j = 1:num_train
         T = trainv(j, :);
         dist = (x' - T')'*(x' - T');
-        if(dist < kSmallest(2, 1))
-            kSmallest(:, end) = [trainlab(j); dist];
-            kSmallest = sortrows(kSmallest.',2).';
+        if(dist < k_smallest(2, 1))
+            k_smallest(:, end) = [trainlab(j); dist];
+            k_smallest = sortrows(k_smallest.',2).';
         end
     end
-    classified(i) = mode(kSmallest(1, :)); 
+    classified(i) = mode(k_smallest(1, :)); 
 end
 
 
